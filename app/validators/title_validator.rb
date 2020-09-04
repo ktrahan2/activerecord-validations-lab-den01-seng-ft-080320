0e1f502875c039f56658173d1eacf7a0d2bcc18d
Kyle Trahan <ktrahan2@gmail.com>
2020-09-04 10:51:42 -0400

    Done.

---
+++ app/validators/title_validator.rb
@@ -0,0 +1,14 @@
+class TitleValidator < ActiveModel::Validator
+
+    def validate(record)
+        if record.title
+            reqs = ["Won't Believe", "Secret", "Top[number]", "Guess"]
+            if reqs.detect {|el| record.title.include?(el) }.nil?
+                record.errors[:title] << "Must contain clickbait"
+            else
+                
+            end
+        end
+    end
+
+end
